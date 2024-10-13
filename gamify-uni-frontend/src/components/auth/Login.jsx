8cbbac8c1773ce79a0e1dad949fba36ac8497fbe
leslie-23 <leslieajayi27@gmail.com>
2024-10-13 18:15:26 +0000

    new adds

--- gamify-uni-frontend/src/components/auth/Login.jsx
+++ gamify-uni-frontend/src/components/auth/Login.jsx
@@ -29,7 +29,7 @@ const Login = () => {
   };
 
   return (
-    <div className="h-screen flex justify-center items-center flex-col md:flex-row bg-slate-400">
+    <div className="h-screen flex justify-center items-center flex-col md:flex-row bg-blue-600">
       <form className="flex justify-center items-center flex-col bg-blue-600 rounded-lg h-3/5 w-1/3">
         <iframe
           className="h-72 w-72"
@@ -37,10 +37,10 @@ const Login = () => {
         ></iframe>
       </form>
       <form
-        className="flex justify-center items-center flex-col bg-blue-300 rounded-lg h-3/5 w-1/3"
+        className="flex justify-center items-center flex-col bg-blue-300 rounded-lg h-3/5 w-1/3 box-shadow-3xl box-shadow-green-600"
         onSubmit={handleSubmit}
       >
-        <h1 className="text-3xl font-bold my-5">Login</h1>
+        <h1 className="text-7xl font-bold my-5">Login</h1>
         <input
           className="outline-gray-200 rounded-lg px-5 py-3 my-2"
           type="email"
@@ -55,12 +55,20 @@ const Login = () => {
           onChange={(e) => setPassword(e.target.value)}
           placeholder="Password"
         />
-        <button
-          className="bg-red-300 rounded-lg px-6 py-3 hover:shadow-xl"
-          type="submit"
-        >
-          Login
-        </button>
+        <div className="row-span-2 gap-3">
+          <button
+            className="bg-blue-600 rounded-lg px-6 py-3 mx-3 hover:shadow-xl"
+            type="submit"
+          >
+            Login
+          </button>
+          <button
+            className="bg-blue-600 rounded-lg px-6 py-3 hover:shadow-xl"
+            type="submit"
+          >
+            <a href="http://localhost:5000/register">Register</a>
+          </button>
+        </div>
       </form>
     </div>
   );

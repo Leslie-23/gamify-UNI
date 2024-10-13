65635316622f5adb162e8ab2a35ab08616f7746d
leslie-23 <leslieajayi27@gmail.com>
2024-10-13 11:53:04 +0000

    added a running page for the backend host

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,17 @@
 const app = require("./app");
 const PORT = process.env.PORT || 5000;
 
+app.get("/", (req, res) => {
+  res
+    .status(200)
+    .json({
+      message: "running",
+      version: "0.0.1",
+      port: `${PORT}`,
+      createdBy: "leslie paul ajayi (PAL tech)",
+    });
+});
+
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log(`\n Server running on port: ${PORT}`);
 });
